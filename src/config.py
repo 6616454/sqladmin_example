a65b672ae76ef9fea511444896dde7bd23cb6fbe
hhssome <artempavel-terekhovy@mail.ru>
2023-02-25 17:18:36 +0300

    Create config loader

--- src/config.py
+++ src/config.py
@@ -2,18 +2,32 @@ from dataclasses import dataclass
 
 from pydantic import BaseSettings
 
+from src.infrastructure.db.config import DBConfig
+from src.presentation.api.config import APIConfig
+
 
 class ConfigExtractor(BaseSettings):
+    title: str
+    database_url: str
+
     class Config:
         env_file = '.env'
 
 
 @dataclass
 class Config:
-    pass
+    api: APIConfig
+    db: DBConfig
 
 
-def load_settings() -> Config:
+def load_config() -> Config:
     config = ConfigExtractor()
 
-    return Config()
+    return Config(
+        api=APIConfig(
+            title=config.title
+        ),
+        db=DBConfig(
+            database_url=config.database_url
+        )
+    )
