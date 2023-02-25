fb31006e6d81a302663d396bcaf00edeab37534c
hhssome <artempavel-terekhovy@mail.ru>
2023-02-25 18:37:34 +0300

    Added controller logic

--- src/presentation/api/controllers/post.py
+++ src/presentation/api/controllers/post.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from src.business_logic.services.posts_get import PostsGetService
+from src.presentation.api.di.providers.services import posts_get_service
 
 router = APIRouter(
     prefix='/posts',
@@ -7,5 +10,5 @@ router = APIRouter(
 
 
 @router.get('/')
-async def get_posts():
-    pass
+async def get_posts(service: PostsGetService = Depends(posts_get_service)):
+    return await service.execute()
