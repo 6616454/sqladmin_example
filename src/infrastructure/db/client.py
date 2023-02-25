c359c2f4915a1c7141f6874e9ce1ada57c227d79
hhssome <artempavel-terekhovy@mail.ru>
2023-02-25 21:21:10 +0300

    Removed session close, close guaranteed

--- src/infrastructure/db/client.py
+++ src/infrastructure/db/client.py
@@ -13,7 +13,6 @@ class DatabaseClient:
     async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         if exc_type:
             await self.rollback()
-        await self._session.close()
 
     async def commit(self) -> None:
         await self._session.commit()
