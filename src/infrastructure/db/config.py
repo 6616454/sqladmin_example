c2088d13ba2cec21af241cf5fecc98a68d26a50e
hhssome <artempavel-terekhovy@mail.ru>
2023-02-25 17:18:25 +0300

    Added fields

--- src/infrastructure/db/config.py
+++ src/infrastructure/db/config.py
@@ -3,4 +3,4 @@ from dataclasses import dataclass
 
 @dataclass
 class DBConfig:
-    pass
+    database_url: str
--- src/presentation/api/config.py
+++ src/presentation/api/config.py
@@ -3,4 +3,4 @@ from dataclasses import dataclass
 
 @dataclass
 class APIConfig:
-    pass
+    title: str
