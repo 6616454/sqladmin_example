4300fa05174ae0c65bf6ff6d6a940248d97bbe8c
hhssome <artempavel-terekhovy@mail.ru>
2023-02-25 18:35:10 +0300

    Renamed class

--- src/infrastructure/db/models/post.py
+++ src/infrastructure/db/models/post.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
-from src.infrastructure.db.models.base import BaseModel
+from src.infrastructure.db.models.base import Base
 
 
-class Post(BaseModel):
+class Post(Base):
     __tablename__ = "posts"
 
     id: Mapped[int] = mapped_column(primary_key=True)
