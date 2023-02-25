4f10478a02d84da8707f0304c81345eeb53de2d4
hhssome <artempavel-terekhovy@mail.ru>
2023-02-25 17:06:32 +0300

    create factories

--- src/infrastructure/db/main.py
+++ src/infrastructure/db/main.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -5,3 +6,9 @@ class Base(DeclarativeBase):
     pass
 
 
+def create_engine(database_url: str) -> AsyncEngine:
+    return create_async_engine(url=database_url)
+
+
+def create_pool(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+    return async_sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
